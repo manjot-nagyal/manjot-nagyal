2c3aa2539c8df80036f9fce0c829c7813a7aeaae
manjot-nagyal <mnagyal@icloud.com>
2025-03-21 11:27:10 -0400

    chore: fix metadata configuration

--- _site/assets/js/search-data.js
+++ _site/assets/js/search-data.js
@@ -1,66 +1,56 @@
 var store = [{
         "title": "Golden Hornet",
-        "excerpt":"Technical Details   Medium: Digital illustration  Software: Sketchbook Pro on a iPad Air with an Apple Pencil  Date completed: 2024-06-26","categories": [],
+        "excerpt":"Technical Details   Medium: Digital illustration  Software: Sketchbook Pro on a iPad Air with an Apple Pencil","categories": [],
         "tags": ["digital art","illustration","creative process"],
-        "url": "http://localhost:4000/art/2024-06-26-hornet/"
+        "url": "https://manjot-nagyal.github.io//manjot-nagyal/art/2024-06-26-hornet/"
       },{
         "title": "Dragonfly",
-        "excerpt":"Technical Details   Medium: Digital illustration  Software: Sketchbook Pro on a iPad Air with an Apple Pencil  Date completed: 2024-06-26","categories": [],
+        "excerpt":"Technical Details   Medium: Digital illustration  Software: Sketchbook Pro on a iPad Air with an Apple Pencil","categories": [],
         "tags": ["digital art","illustration","creative process"],
-        "url": "http://localhost:4000/art/2025-06-26-dragonfly/"
+        "url": "https://manjot-nagyal.github.io//manjot-nagyal/art/2025-06-26-dragonfly/"
       },{
         "title": "Fly",
-        "excerpt":"Technical Details   Medium: Digital illustration  Software: Sketchbook Pro on a iPad Air with an Apple Pencil  Date completed: 2024-06-26","categories": [],
+        "excerpt":"Technical Details   Medium: Digital illustration  Software: Sketchbook Pro on a iPad Air with an Apple Pencil","categories": [],
         "tags": ["digital art","illustration","creative process"],
-        "url": "http://localhost:4000/art/2025-06-26-fly/"
+        "url": "https://manjot-nagyal.github.io//manjot-nagyal/art/2025-06-26-fly/"
       },{
         "title": "Bird",
-        "excerpt":"Technical Details   Medium: Digital illustration  Software: Sketchbook Pro on a iPad Air with an Apple Pencil  Date completed: 2025-02-14","categories": [],
+        "excerpt":"Technical Details   Medium: Digital illustration  Software: Sketchbook Pro on a iPad Air with an Apple Pencil","categories": [],
         "tags": ["digital art","illustration","creative process"],
-        "url": "http://localhost:4000/art/2025-02-14-bird1/"
-      },{
-        "title": "Abstract Painting",
-        "excerpt":"Technical Details   Medium: Watercolor  Date completed: 2025-03-18","categories": [],
-        "tags": ["abstract","painting","creative process"],
-        "url": "http://localhost:4000/art/2025-03-18-abstract/"
+        "url": "https://manjot-nagyal.github.io//manjot-nagyal/art/2025-02-14-bird1/"
       },{
         "title": "Acrylic Bird Painting",
-        "excerpt":"Technical Details   Medium: Acrylic  Date completed: 2025-03-18","categories": [],
+        "excerpt":"Technical Details   Medium: Acrylic","categories": [],
         "tags": ["acrylic","painting","creative process"],
-        "url": "http://localhost:4000/art/2025-03-18-bird2/"
+        "url": "https://manjot-nagyal.github.io//manjot-nagyal/art/2025-03-18-bird2/"
       },{
         "title": "Acrylic Swan Painting",
-        "excerpt":"Technical Details   Medium: Acrylic  Date completed: 2025-03-18","categories": [],
+        "excerpt":"Technical Details   Medium: Acrylic","categories": [],
         "tags": ["acrylic","painting","creative process"],
-        "url": "http://localhost:4000/art/2025-03-18-bird3/"
+        "url": "https://manjot-nagyal.github.io//manjot-nagyal/art/2025-03-18-bird3/"
       },{
         "title": "Charcoal Drawing",
-        "excerpt":"Technical Details   Medium: Charcoal  Date completed: 2025-03-18","categories": [],
+        "excerpt":"Technical Details   Medium: Charcoal","categories": [],
         "tags": ["charcoal","illustration","creative process"],
-        "url": "http://localhost:4000/art/2025-03-18-charcoal/"
+        "url": "https://manjot-nagyal.github.io//manjot-nagyal/art/2025-03-18-charcoal/"
       },{
         "title": "Acrylic Flower Field Painting",
-        "excerpt":"Technical Details   Medium: Acrylic  Date completed: 2025-03-18","categories": [],
+        "excerpt":"Technical Details   Medium: Acrylic","categories": [],
         "tags": ["acrylic","painting","creative process"],
-        "url": "http://localhost:4000/art/2025-03-18-field/"
+        "url": "https://manjot-nagyal.github.io//manjot-nagyal/art/2025-03-18-field/"
       },{
         "title": "Watercolor Sketch of Forest",
-        "excerpt":"Technical Details   Medium: Watercolor and pen  Date completed: 2025-03-18","categories": [],
+        "excerpt":"Technical Details   Medium: Watercolor and pen","categories": [],
         "tags": ["watercolor","illustration","creative process"],
-        "url": "http://localhost:4000/art/2025-03-18-sky/"
-      },{
-        "title": "Acrylic Vase Painting",
-        "excerpt":"Technical Details   Medium: Acrylic  Date completed: 2025-03-18","categories": [],
-        "tags": ["acrylic","painting","creative process"],
-        "url": "http://localhost:4000/art/2025-03-18-vase/"
+        "url": "https://manjot-nagyal.github.io//manjot-nagyal/art/2025-03-18-sky/"
       },{
         "title": "My First Post",
         "excerpt":"Hello world! On this website, I’ll be sharing:   Updates on projects I’m working on  Insights into my creative process  Tutorials and how-to guidesStay tuned! Content will be added soon. ","categories": ["blog"],
         "tags": ["introduction","personal"],
-        "url": "http://localhost:4000/blog/my-first-post/"
+        "url": "https://manjot-nagyal.github.io//manjot-nagyal/blog/my-first-post/"
       },{
         "title": "Microbiome Data Analysis API",
         "excerpt":"Project Overview A containerized REST API for analyzing microbiome data, built with FastAPI. This API allows users to upload microbiome abundance data in JSON or CSV format and perform various diversity and correlation analyses. Features   Data Upload and Management: Support for both CSV and JSON microbiome data formats with sample metadata integration  Diversity Analysis: Calculates alpha diversity metrics (Shannon, Simpson, Pielou, Chao1) and beta diversity metrics (Bray-Curtis, Jaccard)  Correlation Analysis: Analyzes relationships between microbial taxa and sample metadata  Statistical Testing: Performs group comparisons using Mann-Whitney U tests to identify significant differences  Interactive Visualizations: Provides heatmaps, scatter plots, and other visualizations for analyzing microbial communities  RESTful API Documentation: Includes Swagger UI and ReDoc for comprehensive API documentation  Containerized Deployment: Offers Docker-based deployment for easy installation and consistent environmentsDashboard  Links   GitHub Repository","categories": [],
         "tags": ["web development","javascript","react"],
-        "url": "http://localhost:4000/projects/2025-03-18-project1/"
+        "url": "https://manjot-nagyal.github.io//manjot-nagyal/projects/2025-03-18-project1/"
       }]
